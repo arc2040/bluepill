5adb2d3504255faa2edb0c3346ef33b85fb13220
WilliamSmith <izhangtengwei@gmail.com>
2020-05-11 10:22:07 +0800

    serial.read

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,22 @@
 #include <Arduino.h>
 
-#define ANALOG_PIN PA0
-#define PWN_PIN PA1
-void setup(){
 
-    Serial.begin(9600);
-    while(!Serial){;}
+int incomingByte = 0;   // for incoming serial data
 
-    pinMode(LED_GREEN, OUTPUT);
-    Serial.printf("helloWorld\n");
+void setup() {
+        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
 }
 
-void loop(void){
+void loop() {
 
-    analogWrite(PWN_PIN,50);
+        // send data only when you receive data:
+        if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
 
-  int sensorValue = analogRead(ANALOG_PIN);
-  // print out the value you read:
-  Serial.println(sensorValue/4);
-
-    delay(500);
-    digitalWrite(LED_GREEN,HIGH);
-    delay(500);
-    digitalWrite(LED_GREEN,LOW);
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte, DEC);
+                
+        }
 }
